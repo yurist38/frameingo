f2ac1e49a5340dcb19694fc5c8dcfd05d401fc10
Yuri Drabik <yurist38@mail.ru>
2016-02-25 18:48:30 +0700

    fix header and install GTM

--- client/js/controllers/index.js
+++ client/js/controllers/index.js
@@ -1,9 +1,3 @@
 Template.index.rendered = function() {
     $('.content').css('height', ($(window).height() - $('nav').outerHeight()) + 'px' );
 }
-
-Template.mainLayout.helpers({
-    currentUser: function() {
-        return !!Meteor.userId() && !!Template.subscriptionsReady;
-    }
-})
