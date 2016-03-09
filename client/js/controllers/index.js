8eec2f280ce0779b456a1d2b9921a1ff3b9d4e64
Yuri Drabik <yurist38@mail.ru>
2016-03-09 13:29:52 +0700

    add welcome console msg

--- client/js/controllers/index.js
+++ client/js/controllers/index.js
@@ -2,6 +2,16 @@ Template.mainLayout.created = () => {
     TAPi18n.setLanguage(I18NConf.getLanguage());
 };
 
+Template.mainLayout.rendered = function() {
+    let styles = [
+    'background:#98AFC7;color:#ffffff;font-weight:bold;',
+    'background:#2B547E;color:#ffffff;font-weight:bold',
+    'background:#2B3856;color:#ffffff;font-weight:bold;'
+    ];
+    console.log ( '%c Welcome to %c Instaphobia\'s %c Underground!',
+        styles[0], styles[1], styles[2]);
+};
+
 Template.index.events({
     'click .block1': () => {
         Router.go('addevent');
