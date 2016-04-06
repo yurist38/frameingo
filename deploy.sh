23c4b8fa8f487de1388e5e475cebb90bc60639d7
Yuri Drabik <yurist38@mail.ru>
2016-04-06 20:37:17 +0700

    travis change deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ echo "On branch '$TRAVIS_BRANCH'."
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   echo "Triggering Mup deployment..."
-  sudo mupx setup
-  sudo mupx deploy
+  mupx setup
+  mupx deploy
 else
   echo "Not deploying. Use 'master' branch to deploy."
 fi
